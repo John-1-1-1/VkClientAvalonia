8874cf99f247f3d0f023e07f572eccf2b5469db1
John-1-1-1 <52410048+John-1-1-1@users.noreply.github.com>
2022-12-01 02:03:58 +0700

    Change MainWindow

--- VkClientAvalonia.GUI.App/ViewModels/Container.cs
+++ VkClientAvalonia.GUI.App/ViewModels/Container.cs
@@ -1,12 +1,9 @@
 namespace VkClientAvalonia.GUI.App.ViewModels; 
 
 public class Container {
-
-    public static AutorizationViewModel AutorizationViewModel { get; set; }
-
-    public static ListDialogsViewModel ListDialogsViewModel { get; set; }
-
-    public static MessengerViewModel MessengerViewModel { get; set; }
     
-
+    public static AutorizationViewModel AutorizationViewModel { get; set; }
+    public static ListDialogsViewModel ListDialogsViewModel { get; set; }
+    public static MessengerViewModel MessengerViewModel { get; set; }
+    public static MainWindowViewModel MainWindowViewModel { get; set; }
 }
