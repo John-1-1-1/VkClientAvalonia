ac6246a77f55d35e71a1bd06390a71911c7cede8
John-1-1-1 <52410048+John-1-1-1@users.noreply.github.com>
2022-12-01 17:36:12 +0700

    Add Selector

--- VkClientAvalonia.GUI.App/ViewModels/ListDialogsViewModel.cs
+++ VkClientAvalonia.GUI.App/ViewModels/ListDialogsViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive;
+using Avalonia.Controls.Selection;
 using ReactiveUI;
 using VkClientAvalonia.Utils.Containers;
 using VkClientAvalonia.Utils.Vk;
@@ -16,7 +17,18 @@ public class ListDialogsViewModel: ReactiveObject, IListDialogsViewModel {
 
     public List<Dialog> _users;
     public String _userName = "None";
+    
+    public SelectionModel<Dialog> Selection { get; set; }
 
+    private Dialog _selectedItem;
+
+    public Dialog SelectedItem
+    {
+        get => _selectedItem;
+        set => this.RaiseAndSetIfChanged(ref _selectedItem, value);
+    }
+
+    
     public ReactiveCommand<Unit, Unit> Exit { get; set; }
 
     public String UserName {
@@ -28,16 +40,29 @@ public class ListDialogsViewModel: ReactiveObject, IListDialogsViewModel {
         get => _users;
         set => this.RaiseAndSetIfChanged(ref _users, value);
     }
-    
-    
+
     public ListDialogsViewModel() {
         Users = new List<Dialog>();
+        
+        Selection = new SelectionModel<Dialog>();
+
+        Selection.SelectionChanged += SelectedChanged;
+        
         SingletonContainer.GetInstance().GetContainer().AddObject<IListDialogsViewModel>(this);
         Exit = ReactiveCommand.Create( () => 
             SingletonContainer.GetInstance().GetContainer().
                 GetObject<IMainControls>().ShowAutorizationControl());
     }
 
+    private void SelectedChanged(object? sender, SelectionModelSelectionChangedEventArgs<Dialog> e) {
+        var item = ((SelectionModel<Dialog>)sender).SelectedItem;
+        if (item == null) {
+            throw new ArgumentNullException();
+        }
+        
+        
+    }
+    
     public void ShowData() {
 
         var vkClient = SingletonContainer.GetInstance().GetContainer().GetObject<IVkClient>();
